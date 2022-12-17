2b11f622cac05079205a1adbd989d4ed7fc0ffc4
edwin <edwinmarrima@gmail.com>
2022-12-17 07:06:32 -0800

    doc: comment logic behind mergito

--- utils/merge.go
+++ utils/merge.go
@@ -44,17 +44,23 @@ func DeepMerge(src, dst reflect.Value, overrideArray bool) (any, error) {
 		dstMapValue := dst.MapIndex(srcMapKey)
 
 		switch {
+
 		case srcMapValue.Kind() == reflect.Map && dstMapValue.Kind() != reflect.Invalid:
+			//verify if the data type of both maps is the same, if not an error is returned
 			if s := dataTypeValidation(srcMapValue.Type(), dstMapValue.Elem().Type()); s != "" {
 				return nil, fmt.Errorf(s)
 			}
+			//if the elements are a map, we call the function recursively until we reach the level
+			//where the elements are primitive types
 			DeepMerge(srcMapValue, dstMapValue.Elem(), overrideArray)
 			continue
 		case srcMapValue.Kind() == reflect.Slice && dstMapValue.Kind() != reflect.Invalid:
+			// if overrideArray is true, we don't merge(join) array content, instead we override
 			if overrideArray {
 				dst.SetMapIndex(srcMapKey, srcMapValue)
 				continue
 			}
+			//check if the data type of both arrays is the same, if not an error is returned
 			if s := dataTypeValidation(srcMapValue.Type(), dstMapValue.Elem().Type()); s != "" {
 				return nil, fmt.Errorf(s)
 			}
@@ -69,7 +75,7 @@ func DeepMerge(src, dst reflect.Value, overrideArray bool) (any, error) {
 
 func dataTypeValidation(src, dst reflect.Type) string {
 	if src != dst {
-		return fmt.Sprintf("Cannot append two %ss with different type (%s, %s)", src.Kind(), src, dst)
+		return fmt.Sprintf("Cannot append two %ss with different types (%s, %s)", src.Kind(), src, dst)
 	}
 	return ""
 
