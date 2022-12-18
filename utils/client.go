e9784e068284e5fd12548998118326dcef769b6e
edwin <edwinmarrima@gmail.com>
2022-12-18 14:45:23 -0800

    debug(ci/cd): print variable content

--- utils/client.go
+++ utils/client.go
@@ -145,9 +145,9 @@ func (cl Client) dotEnv(b []byte, path, content string) error {
 
 func (cl Client) ReadHandler(path string) (*os.File, error) {
 	dirPath, _ := filepath.Split(path)
-	fmt.Println("AAAAAAAAAAAAA:" + dirPath)
 	// check if directory exists and create new one if not
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		fmt.Println("AAAAAAAAAX:", err)
 		os.Mkdir(dirPath, 0777)
 	}
 
